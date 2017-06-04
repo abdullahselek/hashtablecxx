28438fcd07d0fb15baab2a1896f91037b04a9f64
Abdullah Selek <abdullahselek@gmail.com>
2017-06-04 18:52:47 +0200

    updated put function

--- include/hashtablecxx.h
+++ include/hashtablecxx.h
@@ -21,13 +21,6 @@ public:
 
     void put(const K key, const V value) {
         Node<K, V> *item = new Node<K, V>(key, value);
-        int size = table.size();
-        if (size == 0) {
-            table.push_back(item);
-            return;
-        }
-
-        Node<K, V> *previous = table.at(size - 1);
         table.push_back(item);
     }
 
